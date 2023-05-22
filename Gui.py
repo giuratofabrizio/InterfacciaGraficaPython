bc5f16455b02e8ffafc13b30caf826e5c5cdd52c
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2023-05-22 08:55:28 +0200

    prova

--- Gui.py
+++ Gui.py
@@ -1,1 +1,26 @@
-yoooooooo
+import tkinter as tk
+from tkinter import messagebox
+
+def display_message():
+    name = entry.get()
+    if name:
+        message = f"Hello, {name}!"
+        messagebox.showinfo("Message", message)
+    else:
+        messagebox.showerror("Error", "Please enter a name.")
+
+# Creazione della finestra principale
+window = tk.Tk()
+window.title("Form")
+window.geometry("300x150")
+
+# Creazione del widget Entry
+entry = tk.Entry(window, width=30)
+entry.pack(pady=20)
+
+# Creazione del pulsante
+button = tk.Button(window, text="Submit", command=display_message)
+button.pack()
+
+# Avvio del ciclo principale della finestra
+window.mainloop()
