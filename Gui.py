97b517b16a27236ca5129633c39705db6e9c221a
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2023-05-22 09:43:41 +0200

    Inizio

--- Gui.py
+++ Gui.py
@@ -1,26 +1,37 @@
 import tkinter as tk
 from tkinter import messagebox
+import pymssql as sql
 
-def display_message():
-    name = entry.get()
-    if name:
-        message = f"Hello, {name}!"
-        messagebox.showinfo("Message", message)
-    else:
-        messagebox.showerror("Error", "Please enter a name.")
 
-# Creazione della finestra principale
-window = tk.Tk()
-window.title("Form")
-window.geometry("300x150")
+conn = sql.connect(server='213.140.22.237\SQLEXPRESS', user= 'giurato.fabrizio', password='xxx123##', database='giurato.fabrizio')
+window = tk.tk()
+nome_i = tk.Entry(window)
+cognome_i = tk.Entry(window)
+password_i = tk.Entry(window)
 
-# Creazione del widget Entry
-entry = tk.Entry(window, width=30)
-entry.pack(pady=20)
 
-# Creazione del pulsante
-button = tk.Button(window, text="Submit", command=display_message)
-button.pack()
+def InsertUsers():
+    cursor = conn.cursor()
+    # inserimento dati nella tabella
+    cursor.execute("INSERT INTO utenteGUI VALUES (%(nome_i)s, %(cognome_i)s, %(password_i)s)")
+    conn.commit()
 
-# Avvio del ciclo principale della finestra
-window.mainloop()
+    conn.close()
+
+    button = tk.Button(window, text="Submit", command=InsertUsers)
+    button.pack()
+
+def DeleteUsers():
+    cursor = conn.cursor()
+    # inserimento dati nella tabella
+    cursor.execute("DELETE FROM utenteGUI WHERE nome=%(nome_i)s AND cognome=%(cognome_i)s")
+    conn.commit()
+
+    button = tk.Button(window, text="Inserisci", command=InsertUsers)
+    button.pack()
+
+    button1= tk.Button(window, text="Elimina", command=DeleteUsers)
+    button1.pack()
+    # chiusura connessione al database
+
+    
